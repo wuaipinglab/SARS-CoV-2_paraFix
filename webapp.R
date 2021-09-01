38d02ea88e15dac4a24fd3f818b43b4d59dce4f0
Chengyang Ji <chengyang.ji12@alumni.xjtlu.edu.cn>
2021-09-01 17:40:10 +0800

    Stop using jupyter notebook

--- webapp.R
+++ webapp.R
@@ -1,58 +1,52 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(fs)
 library(shiny)
 library(sitePath)
 
 options(list("shiny.host" = "0.0.0.0",
-             "shiny.port" = 9000))
+             "shiny.port" = 10000))
 
 DATA_DIR <- "webdata"
 SITEPATH_RES_DIR <- file.path(DATA_DIR, "sitePath_results")
+AVAILABLE_DATES <- list.files(SITEPATH_RES_DIR)
 
-availableDate <- list.files(SITEPATH_RES_DIR)
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
     # Application title
     titlePanel("Nextstrain SARS-CoV-2 sites"),
-    
     # Sidebar with a slider input for date
-    sidebarLayout(sidebarPanel(
-        selectInput(
-            inputId = "date",
-            label = "Date:",
-            choices = availableDate,
-            selected = as.character(max(as.Date(availableDate)))
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                inputId = "date",
+                label = "Date:",
+                choices = AVAILABLE_DATES,
+                selected = as.character(max(as.Date(AVAILABLE_DATES)))
+            ),
+            selectInput(
+                inputId = "site",
+                label = "Site:",
+                choices = "all"
+            )
         ),
-        selectInput(
-            inputId = "site",
-            label = "Site:",
-            choices = "all"
-        )
-    ),
-    
-    # Show a plot of the generated plot
-    mainPanel(h4(textOutput(
-        "date"
-    )),
-    plotOutput("distPlot")))
+        # Show a plot of the generated plot
+        mainPanel(
+            h4(textOutput("date")),
+            h5("Fixed sites"),
+            plotOutput("fixedSitesPlot"),
+            h5("Parallel sites"),
+            plotOutput("paraSitePlot")
+        ),
+    )
 )
 
 # Define server logic required to draw the plot
 server <- function(input, output, session) {
     fixedSites <- reactive({
-        # read RDS file from ui.R
         readRDS(file.path(SITEPATH_RES_DIR, input$date, "fixation.rds"))
     })
-    output$distPlot <- renderCachedPlot({
+    paraSites <- reactive({
+        readRDS(file.path(SITEPATH_RES_DIR, input$date, "parallel.rds"))
+    })
+    output$fixedSitesPlot <- renderCachedPlot({
         fixed <- fixedSites()
         # draw the plot based on the site
         site <- input$site
@@ -61,7 +55,16 @@ server <- function(input, output, session) {
         } else {
             plotSingleSite(fixed, site)
         }
-    }, cacheKeyExpr = { c(input$date, input$site) })
+    }, cacheKeyExpr = {
+        c(input$date, input$site)
+    })
+    output$paraSitePlot <- renderCachedPlot({
+        para <- paraSites()
+        # draw the plot based on the site
+        plot(para)
+    }, cacheKeyExpr = {
+        c(input$date, input$site)
+    })
     observe({
         fixed <- fixedSites()
         sites <- allSitesName(fixed)
